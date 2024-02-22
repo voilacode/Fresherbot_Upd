341fe9602457dac8067909516fdce6161fdfad51
shifavoila <d4@voilacode.com>
2024-02-22 11:59:20 +0530

    added nav.js file

---
+++ dist/nav.js
@@ -0,0 +1,20 @@
+function toggleMobileMenu() {
+    const mainMenu = document.getElementById('mainMenu');
+    const menuIcon = document.getElementById('menuIcon');
+    mainMenu.classList.toggle('hidden');
+    // Toggle between icons
+    if (mainMenu.classList.contains('hidden')) {
+        menuIcon.innerHTML = '<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />';
+    } else {
+        menuIcon.innerHTML = '<path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />';
+    }
+}
+
+function closeOtherDropdowns(currentDropdownId) {
+    const dropdowns = document.querySelectorAll('.dropdown-menu');
+    dropdowns.forEach((dropdown) => {
+        if (dropdown.id !== currentDropdownId) {
+            dropdown.classList.add('hidden');
+        }
+    });
+}
